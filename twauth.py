96917076ed2eb623e5f51256169544899dc1acc9
xwu <xingyu.wu@zoominfo.com>
2017-05-18 08:47:03 -0400

    twauth.py changed

--- twauth.py
+++ twauth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # OAuth Setting
-consumer_key = 'cHRGGxRdoEkVT5YT8gUcTiGxC'
-consumer_secret = 'zegKJj8UT087NF3FoAXSbnC3Nq52vsWMZq6o7JZwVp2TMFepTy'
-access_token = '864124034379907072-shxhk8LWamAShdhzw08nQMnTNoC84bV'
-access_secret = 'y8U55x5frallDNEpuU0LdClHP7Sv1ajfYlgGNyjndRz1Y'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
